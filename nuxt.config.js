e860014fd6f754423b772f91443de170fa045c87
Jason <pix3l.icious@gmail.com>
2022-07-10 02:39:51 +0700

    remove dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,8 +20,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  buildDir: "dist",
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 

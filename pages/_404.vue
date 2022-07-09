b6a8a3a536bd4e3cf5fc8d5151fa86a84bc0a8c5
Jason <pix3l.icious@gmail.com>
2022-07-10 00:51:58 +0700

    404 page

---
+++ pages/_404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="space-y-8">
+    <div class="space-y-4">
+      <h1 class="text-center text-4xl font-bold md:text-left">I can't find this page!</h1>
+      <p>
+        What are you doing here? You should go back home to the homepage instead.
+      </p>
+    </div>
+    <div class="mt-2">
+      <nuxt-link to="/regular-items" class="dark:bg-gray-800 md:px-3 md:py-2 px-4 py-3 bg-gray-300 font-semibold dark:hover:bg-gray-700 hover:bg-purple-300 transition ease-in-out duration-300 rounded-xl">
+      🏠 Go home
+    </nuxt-link>
+    </div>
+  </div>
+</template>

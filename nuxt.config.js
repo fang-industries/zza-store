2e87389eecc8284ba89bfc4e0ce0cd9ff08f6f3b
Jason <pix3l.icious@gmail.com>
2022-07-10 02:28:02 +0700

    shit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "zza-store",
+    title: "Master Fang's Dark Market",
     htmlAttrs: {
       lang: "en",
     },
